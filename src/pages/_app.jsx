070a58e19e5f578cd8c82786b2da0bf18fc29c89
Kumar Aditya <kumaraditya18027@gmail.com>
2024-12-07 17:42:06 +0530

    Adding favicon, done  intial SEO

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,11 @@ import "../app/globals.css"; // Import your global CSS file
 function MyApp({ Component, pageProps }) {
   return (
     <section>
-
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>My Perfect Pg</title>
+        <meta name="Perfect Pg" content="Pg NearBy you" />
+      </Head>
       <main>
         <Component {...pageProps} />
       </main>
